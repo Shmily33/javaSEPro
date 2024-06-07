683a67bad7f4947508e4e218dbc77b1a219b3954
33 <1554470705@qq.com>
2024-06-07 15:21:26 +0800

    Initial commit

---
+++ Day01/src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: gs_wang
+ * @Date: 2024/06/07/15:20
+ * @Description:
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: gs_wang
+ * @Date: 2024/06/07/15:17
+ * @Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
